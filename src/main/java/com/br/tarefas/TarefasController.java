bf3589e173450cab9bbb6e269f6965965e411520
babue0 <iandpcarvalho@gmail.com>
2026-02-03 20:24:16 -0300

    tratamento de erro com responseEntity

--- src/main/java/com/br/tarefas/TarefasController.java
+++ src/main/java/com/br/tarefas/TarefasController.java
@@ -1,10 +1,13 @@
 package com.br.tarefas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -14,21 +17,26 @@ public class TarefasController {
   private List<Tarefa> listaTarefas = new ArrayList<>();
 
   @GetMapping("/{id}")
-  public Tarefa recuperarTarefas(@PathVariable Long id) {
+  public ResponseEntity<Tarefa> recuperarTarefas(@PathVariable Long id) {
     Optional<Tarefa> tarefa0p = this.listaTarefas.stream().filter(tarefa1 -> tarefa1.getId() == id).findFirst();
 
-    return tarefa0p.get();
+    return tarefa0p.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
   }
 
   @PostMapping
-  public Tarefa addTarefa(@RequestBody Tarefa tarefa){
+  public ResponseEntity<?> addTarefa(@RequestBody Tarefa tarefa){
+    boolean existe = this.listaTarefas.stream().filter(tarefaLista -> tarefaLista.getId() == tarefa.getId()).findFirst().isPresent();
+    if(existe){
+      return ResponseEntity.badRequest().body(Map.of("menssagem", "tarefa com o id " + tarefa.getId() + " ja existente na lista"));
+    }
     this.listaTarefas.add(tarefa);
-    return tarefa;
+    return ResponseEntity.status(HttpStatus.CREATED).body(tarefa);
   }
 
   @GetMapping
-  public List<Tarefa> recuperaTarefas() {
-    return this.listaTarefas;
+  public ResponseEntity<List<Tarefa>>recuperaTarefas() {
+    return ResponseEntity.ok(this.listaTarefas);
   }
 
 
